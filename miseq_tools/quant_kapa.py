e0ad7da5a29085d74a9f7f96ec39e462f96be4e6
Simon Shen <sshen8@gmail.com>
2024-06-30 20:06:24 -0400

    plot cqs

--- miseq_tools/quant_kapa.py
+++ miseq_tools/quant_kapa.py
@@ -5,6 +5,7 @@ import scipy.stats
 import logging
 from .utils import parse_samplesheet, pooled_bp
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def kapaquant(kapafolder, samplesheet, dilution, standard_bp: int):
     samples = parse_samplesheet(samplesheet)
@@ -60,5 +61,32 @@ def kapaquant(kapafolder, samplesheet, dilution, standard_bp: int):
     conc_size_adjusted = conc * standard_bp / amplicon_sizes # pM
     conc_undiluted = conc_size_adjusted * dilution / 1e3 # nM
     conc_undiluted_mass = conc_undiluted * amplicon_sizes * 617.9 * 1e-6 # ng/uL
+
+    fig, ax = plt.subplots()
+    pools = amplicon_sizes.index.to_series(index=[f'Unkn-{i:02d}' for i in range(1, len(amplicon_sizes) + 1)])
+    unkn_plot = unkn.join(pools, on="Content")
+    std_plot = std.copy()
+    std_plot["Pool label"] = "Standards"
+    data_plot = pd.concat([std_plot, unkn_plot])
+    sns.swarmplot(data=data_plot, x='Pool label', y='Cq', ax=ax, order=["Standards"] + pools.tolist())
+    ax.set_xlabel("")
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+        tick.set_ha('right')
+    range_cq = std.Cq.min(), std.Cq.max()
+    range_cq_diff = range_cq[1] - range_cq[0]
+    ax.set_ylim(range_cq[0] - 0.1 * range_cq_diff, range_cq[1] + 0.1 * range_cq_diff)
+    ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+    # second y-axis
+    ax2 = ax.twinx()
+    ax2.set_ylabel('Concentration (pM)')
+    range_sq = std.SQ.max(), std.SQ.min()
+    range_sq_log = np.log10(range_sq) + 12 # convert M to pM
+    range_sq_log_diff = range_sq_log[0] - range_sq_log[1]
+    ax2.set_ylim(np.power(10, [range_sq_log[0] + 0.1 * range_sq_log_diff, range_sq_log[1] - 0.1 * range_sq_log_diff]))
+    ax2.set_yscale("log")
+    ax2.spines['right'].set_visible(True)
+    fig.savefig('quant_kapa.pdf', bbox_inches='tight')
+    plt.close(fig)
 
     pd.concat([amplicon_sizes, conc_undiluted_mass, conc_undiluted], axis=1, keys=['bp', 'ng/uL', 'nM']).to_csv('quant_kapa.csv')
