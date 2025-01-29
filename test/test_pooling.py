1fe92c3e98d5c6450fd72e26f117dae23eddfab7
Simon Shen <sshen8@gmail.com>
2025-01-29 18:07:02 -0500

    allow last pool to use up as much starting material as needed

--- test/test_pooling.py
+++ test/test_pooling.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from miseq_tools.pooling import _pools
+import pytest
 
-def test_pooling():
+@pytest.mark.parametrize("max_ul_pipettable", [10, 3])
+def test_pooling(max_ul_pipettable):
     num_reads = pd.Series({
         "Sample1": 10000000,
         "Sample2": 20000000,
@@ -10,11 +12,12 @@ def test_pooling():
         "Sample1": 10,
         "Sample2": 8.5,
     })
-    min_ul_pipettable = 2
-    max_ul_pipettable = 10
+    min_ul_pipettable = 1
     pools = _pools(num_reads, concs, min_ul_pipettable=min_ul_pipettable, max_ul_pipettable=max_ul_pipettable)
 
     # check all pipetting volumes are within range
     for pool_i, pool in enumerate(pools, 1):
         for sample, ul in pool.items():
-            assert min_ul_pipettable <= ul <= max_ul_pipettable, f"Pool {pool_i} asks to pipette {ul} uL for {sample}, which is out of range"
+            assert min_ul_pipettable <= ul, f"Pool {pool_i} asks to pipette {ul} uL for {sample}, which is less than the minimum of {min_ul_pipettable} uL"
+            if pool_i < len(pools): # last pool
+                assert ul <= max_ul_pipettable, f"Pool {pool_i} asks to pipette {ul} uL for {sample}, which is more than the maximum of {max_ul_pipettable} uL"
